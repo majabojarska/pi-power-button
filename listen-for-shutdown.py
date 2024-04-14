d7dea10b1045e006fb1890b496fcac9354572432
Maja Bojarska <majabojarska98@gmail.com>
2024-04-14 14:03:16 +0200

    Use --poweroff instead of -h (readability)

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -9,4 +9,4 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(3, GPIO.FALLING)
 
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+subprocess.call(["shutdown", "--poweroff", "now"], shell=False)
