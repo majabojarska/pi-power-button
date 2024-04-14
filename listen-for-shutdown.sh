58ee2a4bfc680c7e01cd56e62422a2979196734e
Maja Bojarska <majabojarska98@gmail.com>
2024-04-14 14:34:14 +0200

    Use --full instead of -f, format

--- listen-for-shutdown.sh
+++ listen-for-shutdown.sh
@@ -12,18 +12,18 @@
 
 # Carry out specific functions when asked to by the system
 case "$1" in
-  start)
-    echo "Starting listen-for-shutdown.py"
-    /usr/local/bin/listen-for-shutdown.py &
-    ;;
-  stop)
-    echo "Stopping listen-for-shutdown.py"
-    pkill -f /usr/local/bin/listen-for-shutdown.py
-    ;;
-  *)
-    echo "Usage: /etc/init.d/listen-for-shutdown.sh {start|stop}"
-    exit 1
-    ;;
+start)
+  echo "Starting listen-for-shutdown.py"
+  /usr/local/bin/listen-for-shutdown.py &
+  ;;
+stop)
+  echo "Stopping listen-for-shutdown.py"
+  pkill --full /usr/local/bin/listen-for-shutdown.py
+  ;;
+*)
+  echo "Usage: /etc/init.d/listen-for-shutdown.sh {start|stop}"
+  exit 1
+  ;;
 esac
 
 exit 0
